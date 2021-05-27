d20e9419608e762e7b48dfa57e1b0537ccd412ec
Joanna <joanna.ziola@efinity.com>
2021-05-27 23:02:24 +0200

    heal your chakra ammm

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Button } from 'react-rainbow-components';
-import { db } from '../../firebase';
+import {Button} from 'react-rainbow-components';
+import {db} from '../../firebase';
+import {Container} from "@chakra-ui/react"
 
 export const StartPage = () => {
-    return <Button onClick={() => db.collection('User').add({ name: 'value' })}>START</Button>;
+  return (
+    <Container centerContent>
+      <Button onClick={() => db.collection('User').add({name: 'value'})}>START</Button>;
+    </Container>
+  )
 };
