9326294c8d8eb06d779072ae9ffc0702e702f7df
Nebra <marcin.pawezka@gmail.com>
2021-05-28 21:15:14 +0200

    add progress bar to profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,6 +8,8 @@ import profile from '../assets/png/profile.png';
 import { SectionInfo } from '../components/SectionInfo';
 import { Carouzel } from '../components/Carouzel';
 import { Box } from '@chakra-ui/react';
+import CustomProgressBar from './CustomProgressBar';
+import cori_cat from '../assets/png/cat/Walk (5).png';
 
 const Wrapper = styled.div`
     display: flex;
@@ -31,7 +33,11 @@ export const Profile = () => {
         <Wrapper>
             <SectionTitle title="Profil" subtitle="W tym miejscu możesz zobaczyć swoje dotychczasowe osiągnięcia." />
             <UserInfo name="Ela Popiel" avatarSrc={profile} />
-            <h1>CatBar</h1>
+            <CustomProgressBar 
+                currentExp={35}
+                levelCapExp={100}
+                avatarIcon={cori_cat}
+            />
             <SectionInfoWrapper>
                 <SectionInfo text="W jaki sposób mogę zbierać punkty?" />
             </SectionInfoWrapper>
